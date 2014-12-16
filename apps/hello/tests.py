e38a93685e98054601a9dbba03b8e9b46ec3a7c8
Sashkow <lykhenko.olexandr@gmail.com>
2014-12-16 23:48:57 +0200

    ticket:1 test main view

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 # Create your tests here.
 class SomeTests(TestCase):
     def test_math(self):
         assert(2+2==4)
+
+class MainViewTests(TestCase):
+	def testMainViewShowsUserInfo(self):
+		response = self.client.get(reverse('main'))
+		self.assertContains(response,"Name: Sahsko Surname: Lykhenko")
--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
+def main(request):
+	return HttpResponse("")
+ 	#return HttpResponse("Name: Sahsko Surname: Lykhenko")
