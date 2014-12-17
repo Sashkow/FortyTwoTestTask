185af2753ecad71e43765dca642c33097049bfe5
Sashkow <lykhenko.olexandr@gmail.com>
2014-12-17 11:22:31 +0200

    ticket:1 testUserHasNeededProfileAttributes red

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.test import Client
+from django.contrib.auth.models import User
 
 # Create your tests here.
 class SomeTests(TestCase):
@@ -10,11 +11,25 @@ class SomeTests(TestCase):
 class MainViewTests(TestCase):
 	fixtures =['hello_main_view_testdata.json']
 
+	def __init__(self, *args, **kwargs):
+		super(TestCase, self).__init__(*args, **kwargs)
+		# self.gen_stubs()
+
+		self.c = Client()
+		self.c.login(username='sashko', password='poland')
+		
 	def testMainViewShowsUserInfo(self):
-		c = Client()
-		c.login(username='sashko', password='poland')
-		response = c.get(reverse('main'))
+		response = self.c.get(reverse('main'))
 		print '-->', response, '<--'
 		self.assertContains(response,"Name: Olexandr")
 		self.assertContains(response,"Surname: Lykhenko")
 		self.assertContains(response,"Email: lykhenko.olexandr@gmail.com") 
+
+	def testUserHasNeededProfileAttributes(self):
+		u = User.objects.get(username='sashko')
+		self.assertEquals(hasattr(u, 'bio'),True)
+		self.assertEquals(hasattr(u, 'jabber',True))
+		self.assertEquals(hasattr(u, 'skype',True))
+		self.assertEquals(hasattr(u, 'other_contacts',True))
+		self.assertEquals(hasattr(u, 'birth_date',True))
+
