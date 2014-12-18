c81f6968c8283aa5aee9869846b13f175df679d3
Sashkow <lykhenko.olexandr@gmail.com>
2014-12-18 23:43:27 +0200

    ticket:1 linked main view to root url

--- apps/hello/urls.py
+++ apps/hello/urls.py
@@ -5,6 +5,5 @@ from apps.hello import views
 
 
 urlpatterns = patterns('',
-    # url(r'^$',views.main,name='main'),
     url(r'^$',views.main,name='main'),
 )
--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from apps.hello import views
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hello/', include("apps.hello.urls")),
+    url(r'^$',views.main,name='main'),
 )
