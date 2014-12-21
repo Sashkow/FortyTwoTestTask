24dc98c40989345f43bd2b956c6076baac61adb2
Sashkow <lykhenko.olexandr@gmail.com>
2014-12-22 00:48:41 +0200

    ticket:3 testRequestInfoModelUpdates green

--- apps/hello/middleware.py
+++ apps/hello/middleware.py
@@ -1,9 +1,11 @@
 from models import RequestInfo
 
+import pickle
+
 
 class RequestsToDataBase(object):
 
-	def process_request(self,request):
-		print "REQUEST:", type(request), request
-
-
+	def process_request(self, request):
+		# pickledRequest = pickle.dumps(request)
+		ri = RequestInfo(pickled_request = "pickledRequest")
+		ri.save()
