41186468c0cf2d20b2b72877c179fe8c6417283e
Sashkow <lykhenko.olexandr@gmail.com>
2014-12-21 20:37:40 +0200

    Revert "ticket:1 deleted print messages"
    This reverts commit 65418aad07a862f78174c4fe8148f393db2f5bcf.

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -103,11 +103,12 @@ MEDIA_URL = '/uploads/'
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+print "STATIC_ROOT", STATIC_ROOT
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-
+print "STATIC_URL", STATIC_URL
 
 # Additional locations of static files
 STATICFILES_DIRS = (
