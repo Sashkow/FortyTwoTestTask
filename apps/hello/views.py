572e6df8bf96afebd19517a2c2f29054f3d497ef
Sashkow <lykhenko.olexandr@gmail.com>
2014-12-27 13:00:37 +0200

    Revert "ticket:5 'Create page with a form that allows to edit data, presented on the main page' task part done with tests green"
    This reverts commit b7a7b505c98484790592f81f273f3f3d65511b62.

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,23 +1,19 @@
-
-
-
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from django.core.urlresolvers import reverse
+from django.http import HttpResponse
 from django.contrib.auth.models import User
-from models import RequestInfo, UserProfile
+from models import RequestInfo
 from django.contrib.auth import authenticate, login
 
 from django.conf import settings
 from django.template import loader
 from django.template import RequestContext, Context
+
+
 # from django.core.exceptions import DoesNotExist
 
-from apps.hello.forms import UserProfileMultiForm, UserProfileForm, UserForm
 
-from django.utils.safestring import mark_safe
-
-def getUserOrAuthAndGetAdmin(request):
+# Create your views here.
+def main(request):
     userLazySimpleObject = request.user
     try:
         u = User.objects.get(id=userLazySimpleObject.id)
@@ -26,14 +22,9 @@ def getUserOrAuthAndGetAdmin(request):
         u = authenticate(username='admin', password='admin')
         login(request, u)
     finally:
-        return u
-
-# Create your views here.
-def main(request):
-    u = getUserOrAuthAndGetAdmin(request)
-    template_name = 'hello/index.html'
-    context = {'u': u}
-    return render(request, template_name, context)
+        template_name = 'hello/index.html'
+        context = {'u': u}
+        return render(request, template_name, context)
 
 
 def showFirstRequests(request):
@@ -54,28 +45,5 @@ def showDjangoSettings(request):
     
     return render(request,template_name,c)
 
-
-##########################################
-#5
-##########################################
-
 def editUserInfo(request):
-    user = getUserOrAuthAndGetAdmin(request)
-    if request.method == 'POST':
-        form = UserProfileMultiForm(request.POST,instance={
-            'user': user,
-            'profile': user.userprofile,
-            })
-        if form.is_valid():
-            form.save()
-        return HttpResponseRedirect(reverse('main'))
-    else:
-        form = UserProfileMultiForm(instance={
-            'user': user,
-            'profile': user.userprofile,
-            })
-    context = Context({'form': form})
-    return render(request, 'hello/editform.html', context)
-
-def thanks(request):
-    return HttpResponse('thanks!')
+    return HttpResponse("")
--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from apps.hello import views
 
+
+admin.autodiscover()
+print admin.__file__
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hello/', include("apps.hello.urls")),
